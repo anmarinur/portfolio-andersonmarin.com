653fa2bfc281d232ded7a68fc1197ce7e1a840fc
anmarinur <andersonmarindev@gmail.com>
2023-05-18 17:40:42 -0500

    Add profile data and style

--- src/assets/Components/Profile.jsx
+++ src/assets/Components/Profile.jsx
@@ -1,5 +1,16 @@
+import './Profile.css'
+
 export function Profile() {
   return (
-    <h2>Profile</h2>
+    <div className='container-profile'>
+      <div className='container-profile-logo'>
+        <div className='container-profile-headers'>
+          <h1 className='h1-profile'>Anderson Marín</h1>
+          <h2 className='h2-profile'>Full Stack Web Developer</h2>
+        </div>
+        <image alt="logo" src="#"/>
+      </div>
+      <p className='p-profile'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, omnis quasi perspiciatis impedit praesentium vero aliquam, natus voluptatibus a deleniti nisi dolore qui temporibus nobis obcaecati eveniet ducimus officia inventore.</p>
+    </div>
   );
 }
