939fd3c777c3e0350319b0a8ad3b6a580f1563bd
anmarinur <andersonmarindev@gmail.com>
2023-05-27 12:24:30 -0500

    Add effect to portfolio card

--- src/assets/Components/CardPortfolio.jsx
+++ src/assets/Components/CardPortfolio.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './CardPortfolio.css';
 
 export function CardPortfolio({ name, link, repository, image, description, technologies }) {
+
   return (
     <div className="container-card-portfolio">
-      <h2 className="container-card-portfolio-title">{name}</h2>
-      <img className="container-card-img" src={image} alt={name} />
-      <p className="container-card-portfolio-p">{description}</p>
+      <figure>
+        <img className="container-card-img" src={image} alt={name} />
+        <div className="container-card-capa">
+          <h2 className="container-card-portfolio-title">{name}</h2>
+          <p className="container-card-portfolio-p">{description}</p>
+        </div>
+      </figure>
+      <div className="container-card-links">
+        <a href={repository} target='_blanck'><img src="/github.svg" alt="Ícono github" /><span>Repository</span></a>
+        <a href={link} target='_blanck'><img src="/www.svg" alt="Ícono Internet" /><span>Demo</span></a>
+      </div>
     </div>
   );
 }
