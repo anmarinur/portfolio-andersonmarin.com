3612ef14f36a6a62e1da7b7761e9070ed75b65f5
anmarinur <andersonmarindev@gmail.com>
2023-05-30 06:01:32 -0500

    Add new behavior for small devices

--- src/assets/Components/CardPortfolio.jsx
+++ src/assets/Components/CardPortfolio.jsx
@@ -2,13 +2,34 @@ import './CardPortfolio.css';
 
 export function CardPortfolio({ name, link, repository, image, description, technologies, color }) {
 
+  let htmlTech = '';
+  let colorTags = '';
+
+  technologies.map((tech, i) => (
+    i < technologies.length - 1 ? htmlTech += '#' + tech + ', ' : htmlTech += '#' + tech 
+  ))
+
+  if (color == 'white') {
+    colorTags = 'yellow';
+  }
+
   return (
     <div className="container-card-portfolio">
+      <div className='container-card-portfolio-sm'>
+        <h2 className={`container-card-portfolio-title`}>{name}</h2>
+        <p className={`container-card-portfolio-p`}>{description}</p>
+        <p className={`container-card-portfolio-tech`}>           
+          {htmlTech}
+        </p>
+      </div>
       <figure>
         <img className="container-card-img" src={image} alt={name} />
         <div className="container-card-capa">
           <h2 className={`container-card-portfolio-title ${color}`}>{name}</h2>
           <p className={`container-card-portfolio-p ${color}`}>{description}</p>
+          <p className={`container-card-portfolio-tech ${colorTags}`}>           
+            {htmlTech}
+          </p>
         </div>
       </figure>
       <div className="container-card-links">
